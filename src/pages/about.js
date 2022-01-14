e5ec03c942d1c08bd1d24f37696dbcec50d1a53a
esibirkin <88534794+esibirkin@users.noreply.github.com>
2022-01-14 22:42:30 +0300

    Третье изменение файл

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby'
 import * as React from 'react'
 const AboutPage = () => {
     return (
         <main>
             <title>About Me</title>
             <h1>About Me</h1>
+            <Link to="/">Back to Home</Link>
             <p>Hi there! I'm the proud creator of this site</p>
 
         </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import * as React from "react"
-
+import {Link} from "gatsby"
 const IndexPage = () => {
   return (
     <main> 
       <title>Yjmt Pages</title>
       <h1> Welcam to my Gatsby site </h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following rhe gatsby Tutorial.</p>
     </main>
   )
