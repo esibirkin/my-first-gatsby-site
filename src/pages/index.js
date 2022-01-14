1ca3094f6ba6919824cdad3f2f517fa8ccede083
esibirkin <88534794+esibirkin@users.noreply.github.com>
2022-01-14 23:34:03 +0300

    Пятая фиксация

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import * as React from "react"
-import {Link} from "gatsby"
+import Layout from '../components/layout'
 const IndexPage = () => {
   return (
-    <main> 
-      <title>Yjmt Pages</title>
-      <h1> Welcam to my Gatsby site </h1>
-      <Link to="/about">About</Link>
+    <Layout pageTitle="Home Page">  
       <p>I'm making this by following rhe gatsby Tutorial.</p>
-    </main>
+    </Layout>
   )
 }
 
