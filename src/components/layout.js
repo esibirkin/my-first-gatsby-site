cb7e75c0a4d0ee1a56f514887d20947780d3c86f
esibirkin <88534794+esibirkin@users.noreply.github.com>
2022-01-16 20:41:03 +0300

    запрос данных с graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,7 +36,12 @@ const Layout = ({ pageTitle, children}) => {
                             About
                         </Link>
                     </li>
-                    
+                    <li className={navLinkItem}>
+
+                        <Link to='/blog' className={navLinkText}>
+                            Blog
+                        </Link>
+                    </li>
                 </ul>
             </nav>
             <main>
