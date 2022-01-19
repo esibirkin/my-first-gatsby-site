7e5cb566765c69b1384b53546e3aa754b6384c60
esibirkin <88534794+esibirkin@users.noreply.github.com>
2022-01-19 20:38:27 +0300

    Завершён 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,19 +1,19 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import { graphql, Link } from 'gatsby'
+import Layout from '../../components/layout'
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Post">
             {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+              {node.frontmatter.title}
+              </Link>              
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
 
-           <MDXRenderer>
-             {node.body}
-           </MDXRenderer> 
           </article>
         ))
       }
@@ -30,7 +30,8 @@ query {
           title
         }
         id
-        body
+        slug
+      
       }
     }
   }
