578784d52afd5bbe6ec30754d74fcb14acd895b5
esibirkin <88534794+esibirkin@users.noreply.github.com>
2022-01-25 17:22:56 +0300

    исправлено

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,16 @@ module.exports = {
         title: "My First Gatsby Site",
     },
     plugins: [
-       
+        "gatsby-plugin-image",
         "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
     {
         resolve: "gatsby-source-filesystem",
         options: {
             name: `blog`,
             path: `${__dirname}/blog`,
         }
- 
     },
         "gatsby-plugin-mdx",
-        "gatsby-plugin-image",
-               
+        "gatsby-transformer-sharp",      
 ],
 };
